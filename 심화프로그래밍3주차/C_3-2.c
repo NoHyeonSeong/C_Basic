fcacc6eca929925da9a716194ac19b46b8db8c4e
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-09-20 10:19:22 +0900

    Add files via upload

---
+++ 심화프로그래밍3주차/C_3-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a, b;
+
+	puts("2개의 정수를 입력하세요.");
+	printf("정수 A : ");	scanf("%d", &a);
+	printf("정수 B : ");	scanf("%d", &b);
+
+	if(a % b)
+		puts("B는 A의 약수가 아닙니다.");
+	else
+		puts("B는 A의 약수입니다.");
+	return EXIT_SUCCESS;
+}
