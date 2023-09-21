cb6bfc4f83462b270b78696f8c2d7ff87e824b64
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-09-21 09:38:46 +0900

    Add files via upload

---
+++ 심화프로그래밍3주차/C_3-15.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int no;
+
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &no);
+
+	switch(no % 2){
+	case 0 : puts("이 값은 짝수입니다.\n"); break;
+	case 1 : puts("이 값은 홀수입니다.\n"); break;
+	}
+
+	return EXIT_SUCCESS;
+}
