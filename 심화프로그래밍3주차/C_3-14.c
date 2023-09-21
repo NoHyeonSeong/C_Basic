f5c0b70deb5e5f4c8a4905f8f38357f444d2e62f
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-09-21 09:29:17 +0900

    Add files via upload

---
+++ 심화프로그래밍3주차/C_3-14.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a, b;
+	int max, min;
+	puts("2개의 정수를 입력하세요.");
+	printf("정수 A : ");	scanf("%d", &a);
+	printf("정수 B : ");	scanf("%d", &b);
+
+	if(a == b){
+		puts("두 값은 같습니다.");
+	}
+	else{
+		int min, max;
+		if(a < b){
+			min = a;
+			max = b;
+		}
+		else{
+			min = b;
+			max = a;
+		}
+		printf("작은 값은 %d입니다.\n", min);
+		printf("큰 값은 %d입니다.\n", max);
+
+	}
+
+	return EXIT_SUCCESS;
+}
