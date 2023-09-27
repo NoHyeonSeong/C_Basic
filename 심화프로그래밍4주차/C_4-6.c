f8ee9a2d191648a9a83cdf7e7503c19064870c78
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-09-27 10:50:16 +0900

    Add files via upload

---
+++ 심화프로그래밍4주차/C_4-6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i, no;
+
+	printf("양의 정수를 입력하세요: ");
+	scanf("%d", &no);
+
+	if(no >= 1){
+		int i = 2;
+		while(i <= no){
+			printf("%d ", i);
+		i += 2;
+		}
+		printf("\n");
+	}
+	return 0;
+}
