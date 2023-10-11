fff7aae88a88e248f70613c72cc6a2c81cad8021
NoHyeonSeong <143773684+NoHyeonSeong@users.noreply.github.com>
2023-10-11 10:36:46 +0900

    Add files via upload

---
+++ 심화프로그래밍6주차/C_6-4.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int sqr(int x){
+	return x * x;
+}
+
+int pow4(int x){
+	return sqr(x) * sqr(x);
+}
+
+int main(void) {
+	int n;
+
+	printf("정숫값 : ");
+	scanf("%d", &n);
+
+	printf("%d의 4제곱 값은 %d입니다.\n", n, pow4(n));
+	return 0;
+}
